09549a9be7173e417331ef73b74cad748d0ceb18
Matt <matiaslucero21884@gmail.com>
2024-07-14 20:26:24 -0300

    Modificando js

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ function calcularIMC() {
 
     if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
         document.getElementById('resultado').innerText = 'Por favor, introduce valores válidos para altura y peso.';
-    } else if (altura > 1.9 && peso > 901.9) {
+    } else if (altura > 1.85 && peso > 90.9) {
         document.getElementById('resultado').innerText = 'Usted es un anormal y un lechón listo para la parrilla.';
-    } else if (altura > 1.9 && peso < 50) {
+    } else if (altura > 1.85 && peso < 60) {
         document.getElementById('resultado').innerText = 'Usted es un anormal y una lagrima.';
-    } else if (altura < 1.4 && peso > 90) {
+    } else if (altura < 1.6 && peso > 80) {
         document.getElementById('resultado').innerText = 'Usted es un enano y un lechón listo para la parrilla.';
-    } else if (altura < 1.4 && peso < 50) {
+    } else if (altura < 1.6 && peso < 60) {
         document.getElementById('resultado').innerText = 'Usted es un enano y una lagrima.';
     } else if (altura > 2) {
         document.getElementById('resultado').innerText = 'Usted es un anormal.';
-    } else if (altura < 1.4) {
+    } else if (altura < 1.6) {
         document.getElementById('resultado').innerText = 'Usted es un enano.';
-    } else if (peso > 90) {
+    } else if (peso > 80) {
         document.getElementById('resultado').innerText = 'Usted es un lechón listo para la parrilla.';
-    } else if (peso < 50) {
+    } else if (peso < 60) {
         document.getElementById('resultado').innerText = 'Usted es una lagrima.';
     } else {
         const imc = peso / (altura * altura);
